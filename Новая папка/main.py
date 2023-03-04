64a6872b5ffd1b3cea1422c3667f6662ce370b42
Bagdan07 <126941650+Bagdan07@users.noreply.github.com>
2023-03-04 11:22:36 +0500

    uy ishi python

---
+++ Новая папка/main.py
@@ -0,0 +1,35 @@
+# Topshiriq-1_Foydalanuvchi ismini..... terminalga utish
+# fristname = input ("Ismizini kiriting")
+# lastname = input ("familyangizni kiriting")
+# age = ("yoshizni kiriting")
+# email = ("email akauntizni kiritng")
+# password = int(input("parolizni kiritng")
+# print (fristname, lastname, age, email, password)
+ # 1) misolorni yechish
+a = (1/4)+(1/6)-(2/3)-(1/2)
+print(a)
+b = (4/7)-(3/2)*(1/130)*(1/4)
+print(b)
+d = (4*(2/3)+(1/4)*1*(7/9)-(1/9))
+print(d)
+e = (5*(1/7)-(1/7)*1*(3/4)+ (1/4))
+print(e)
+# 2) masalalar yechish
+a=7
+y=9
+c = (a**2)-(y**2)
+n=23
+m=26
+a = (a**2)-(y**2)
+k=76
+n=67
+d = (k**2)-(n**2)
+b=45
+c=54
+s = (b**2)-(c**2)
+p=12
+q=21
+e = (p**2)-(q**2)
+i=77
+k=66
+f = (i**2)-(k**2)
