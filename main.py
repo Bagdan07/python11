afa866bfb043fd9c82fb95b59ff7995f1a62a9e0
Bagdan07 <126941650+Bagdan07@users.noreply.github.com>
2023-03-14 10:47:50 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+# Birinchisi
+# print("Assalomu aleykum"[::-1])
+# ikinchisi
+# print("1abc2das3dbc4ghj5"[::4])
+# uchinchi
+# def replace_python_with_java(text):
+# return text.replace("Python", "JavaScript")
+# input_text = "Python is most popular programming language"
+# output_text = replace_python_with_java(input_text)
+# print(output_text)
+# turtinchi birinchi funksiya
+#def salomber():
+#    print("asalomu aleykum")
+#salomber() 
+# turtinchi ikinchi funksiyasi
+# def obhavo(gradus):
+#     if gradus <=10:
+#         return "obhavo salqin"
+#     elif 10 < gradus <= 25:
+#         return "obhavo iliq"
+#     else:
+#         return "obhavo issiq" 
+
+# print(obhavo(35))
+
+# turtinchisini uchinchi funksiyasi
+# def ovqatber():
+#     print("master kebabdan burger ol va yee")
+# ovqatber()
+# turtinchini turtinchi funksiya
+# def kirish():
+#     print("Admin: salom bizning kompaniyamizga hush kelipsiz, Parolni kiriting:123456789, emailni kiriting:nuranovbagdan034@gmail.com, Ismni kiriting:Bagdan, Admin: hammasi tug'ri dastuimizga kirish mumkin, OMAD!!! ")
+# kirish()          
+
+    
+    
